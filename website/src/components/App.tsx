c92329771f371a80e649ef2ae17fb0abf2b91b95
satsinush <aneedham0577@gmail.com>
2026-05-06 22:42:41 -0500

    Added asset refs to export

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -838,6 +838,7 @@ export default function App() {
         if (notebookMetadata.team.logo) teamImages.push(notebookMetadata.team.logo);
         notebookMetadata.team.members.forEach(m => { if (m.image) teamImages.push(m.image); });
         for (const imgPath of teamImages) await fetchAsset(imgPath);
+        portable.assetRefs = notebookMetadata.assetRefs;
       }
 
       const blob = new Blob([JSON.stringify(portable, null, 2)], { type: "application/json" });
