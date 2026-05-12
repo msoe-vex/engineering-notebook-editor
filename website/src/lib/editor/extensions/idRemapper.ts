e2635ed38222726aa838e7e3cf7260cc921f100a
satsinush <aneedham0577@gmail.com>
2026-05-12 16:17:13 -0500

    Made sure drag and drop doesn't change uuids

--- website/src/lib/editor/extensions/idRemapper.ts
+++ website/src/lib/editor/extensions/idRemapper.ts
@@ -10,7 +10,26 @@ export const IdRemapper = Extension.create({
       new Plugin({
         key: new PluginKey('idRemapper'),
         props: {
+          handleDOMEvents: {
+            dragstart: (view) => {
+              (view as any).isDraggingFromHere = true;
+              return false;
+            },
+            dragend: (view) => {
+              (view as any).isDraggingFromHere = false;
+              (view as any).isInternalMove = false;
+              return false;
+            },
+            drop: (view, event) => {
+              // This runs before transformPasted
+              (view as any).isInternalMove = (view as any).isDraggingFromHere && !event.ctrlKey && !event.altKey && !event.metaKey;
+              return false;
+            }
+          },
           transformPasted: (slice: Slice): Slice => {
+            if ((this.editor.view as any).isInternalMove) {
+              return slice;
+            }
             const json = slice.content.toJSON();
             const remapped = remapContentIds(json);
             try {
